9cdd69212d2b8742d061b7b3b455b510bd7d8a26
Andrei <anddfedotov@gmail.com>
2019-10-27 23:50:33 -0700

    Move constants to separate file

---
+++ src/main/java/com/qube/nbtparser/TagConstants.java
@@ -0,0 +1,20 @@
+package main.java.com.qube.nbtparser;
+
+public final class TagConstants {
+    private TagConstants() {}
+
+    public static final int END_TAG = 0,
+            BYTE_TAG = 1,
+            SHORT_TAG = 2,
+            INT_TAG = 3,
+            LONG_TAG = 4,
+            FLOAT_TAG = 5,
+            DOUBLE_TAG = 6,
+            BYTE_ARRAY_TAG = 7,
+            STRING_TAG = 8,
+            LIST_TAG = 9,
+            COMPOUND_TAG = 10,
+            INT_ARRAY_TAG = 11,
+            LONG_ARRAY_TAG = 12;
+
+}
